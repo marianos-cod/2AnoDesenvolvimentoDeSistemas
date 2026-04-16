d43633a07b611da231255203c572a56180f07604
marianos-cod <redlightytofc@gmail.com>
2026-04-16 15:51:54 -0300

    .

---
+++ Atividades SENAI/javascript/aula 1 - introducao/scripts/aula.js
@@ -0,0 +1,32 @@
+// resposta no console
+console.log ("Hello Word");
+
+// // caixas de dialogo
+// alert ("Front é massa");
+// prompt("Qual é o seu nome?");
+// confirm("É o menininho de vó é?")
+
+// acessa documento, busca op elemento com o id selecionado
+// inner = dentro
+document.getElementById("resposta").innerText = "<h1>Mariano é massa</h1>;"
+document.getElementById("resposta").innerHTML = "<h1>Mariano é massa</h1>;"
+
+// string = caractere ("Mariano")
+var nome = "Mariano";
+
+// boolean = logico ('verdadeiro' - true, 'falso' - false)
+var ligado = true;
+
+// number = número ( 80 )
+// *obs para numeros quebrados - float
+// *obs para numeros inteiros - int()
+var idade = 16;
+var dinheiro = 154.98;
+
+// array - lista , vetor
+var listaCompras = []
+console.log(typeof(nome));
+console.log(typeof(ligado));
+console.log(typeof(idade));
+console.log(typeof(dinheiro));
+console.log(typeof(listaCompras));
---
+++ Atividades SENAI/javascript/aula 1 - introducao/scripts/usuario.js
@@ -0,0 +1,4 @@
+var nome = prompt("Qual é o seu nome?")
+
+// document.getElementById("resposta").innerHTML = "<h1>"+"Olá"+nome+"</h1>;"
+document.getElementById("resposta").innerHTML = `<h1> ${1+1}  </h1>`;
