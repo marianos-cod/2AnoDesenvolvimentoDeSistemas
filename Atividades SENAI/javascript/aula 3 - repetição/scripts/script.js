64117a8e9e60293792d601761d4532bf9bbaf451
marianos-cod <redlightytofc@gmail.com>
2026-05-05 17:03:39 -0300

    aula 3 - repeticao

---
+++ Atividades SENAI/javascript/aula 3 - repetição/scripts/script.js
@@ -0,0 +1,27 @@
+function aula(){
+    // enquanto () faca ...
+    // para con ate con2 interable
+    var i = 0;
+
+    while (i < 10) {
+        console.log(i);
+        i++;
+    }
+
+    while (true) {
+        var resposta = prompt("Repete? ");
+        
+        if (resposta === false) {
+            break;
+        }
+    }
+    for (var i;i<100,i++){
+        document.querySelector("body").innerHTML += "<p>" + i + "</p>";
+    }
+    // faz e verufica (aceita false)
+    do {
+    console.log(i);
+        i++;
+     
+}
+aula();
