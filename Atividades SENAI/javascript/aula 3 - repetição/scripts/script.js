959b6ab4d5c7c5ba4ffec2899832f04748e80281
marianos-cod <redlightytofc@gmail.com>
2026-05-05 18:15:02 -0300

    aula 3 - repeticao

--- Atividades SENAI/javascript/aula 3 - repetição/scripts/script.js
+++ Atividades SENAI/javascript/aula 3 - repetição/scripts/script.js
@@ -15,13 +15,26 @@ function aula(){
             break;
         }
     }
-    for (var i;i<100,i++){
+    for (var i=0;i<100;i++){
         document.querySelector("body").innerHTML += "<p>" + i + "</p>";
     }
     // faz e verufica (aceita false)
-    do {
-    console.log(i);
-        i++;
-     
+    // do {
+    // console.log(i);
+    //     i++;
+    // }  
 }
-aula();
+function produtos(){
+    for(var i = 0; i < 10; i++){
+        document.querySelector("#produtos").innerHTML += `
+        <div class="produto">
+            <img src="https://images.tcdn.com.br/img/img_prod/1176782/pirulito_psicodelico_colorido_g_c_01_unid_5528_1_6c0be476613bc11e7b2f8ce2ddf0c778.jpg" alt="">
+            <h1>PIRULITO</h1>
+            <p>R$ 20,00</p>
+            <button type="button">COMPRAR</button>
+            <button type="button">ADICIONAR AO CARRINHO</button>
+        </div>
+        `;
+    }
+}
+produtos();
